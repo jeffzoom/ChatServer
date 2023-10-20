0038ca80a1f840c3df08d6a2b34cb559405e2614
zhangshiqian <zhshiqian@163.com>
2023-10-20 10:54:53 +0800

    2023.10.20-chatserver

--- myChatServer/build/src/cmake_install.cmake
+++ myChatServer/build/src/cmake_install.cmake
@@ -40,6 +40,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/server/cmake_install.cmake")
+  include("/home/nowcoder/Linux_nc/ChatServer/myChatServer/build/src/client/cmake_install.cmake")
 
 endif()
 
